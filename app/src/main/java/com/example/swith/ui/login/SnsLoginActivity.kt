0c9295311600c9883afd0e5d421b0f1555193471
kimdoori <dooooreeee@naver.com>
2022-07-18 15:05:17 +0900

    [ADD] snsLoginActivity화면

---
+++ app/src/main/java/com/example/swith/ui/login/SnsLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.swith.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.swith.R
+
+class SnsLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sns_login)
+    }
+}
