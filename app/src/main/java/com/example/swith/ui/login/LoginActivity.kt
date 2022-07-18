afa2d6ad17e6313aa646dc1e8bd11e77c8a9ed2f
kimdoori <dooooreeee@naver.com>
2022-07-18 14:54:31 +0900

    [ADD] login화면, 색 설정을 위해 themes 수정

---
+++ app/src/main/java/com/example/swith/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.swith.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.swith.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
