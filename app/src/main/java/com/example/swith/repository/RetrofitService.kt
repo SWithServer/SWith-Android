06ec6666af407ad20eeb90b7ec76bf15c2bde117
active_dg <nerw173@gmail.com>
2022-07-18 19:39:33 +0900

    [UPD] 회차 생성 Activity 업데이트

--- app/src/main/java/com/example/swith/repository/RetrofitService.kt
+++ app/src/main/java/com/example/swith/repository/RetrofitService.kt
@@ -4,7 +4,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitApi {
+object RetrofitService {
     const val baseUrl = " "
 
     val retrofit : Retrofit by lazy {
