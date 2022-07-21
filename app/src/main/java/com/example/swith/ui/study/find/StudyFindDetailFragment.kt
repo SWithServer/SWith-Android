815e2fe597fd846411889d7d133b531276f5bbd8
ss99x2002 <ss99x200211@gmail.com>
2022-07-21 16:42:45 +0900

    [ADD] 스터디찾기- 스터디 detail Fragment

---
+++ app/src/main/java/com/example/swith/ui/study/find/StudyFindDetailFragment.kt
@@ -0,0 +1,16 @@
+package com.example.swith.ui.study.find
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.swith.R
+import com.example.swith.databinding.FragmentStudyFindDetailBinding
+import com.example.swith.ui.BaseFragment
+
+class StudyFindDetailFragment : BaseFragment<FragmentStudyFindDetailBinding> (R.layout.fragment_study_find_detail) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
