cceedaea0b12ab1863d5fef3e75c7b28c6b9c55e
nerw173 <nerw173@gmail.com>
2022-07-23 20:08:18 +0900

    [UPD] Round Create 시간 선택 업데이트(3)

--- app/src/main/java/com/example/swith/ui/study/create/RoundCreateActivity.kt
+++ app/src/main/java/com/example/swith/ui/study/create/RoundCreateActivity.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.view.MotionEvent
 import android.view.View
@@ -101,6 +102,24 @@ class RoundCreateActivity : AppCompatActivity() {
             etCreatePlace.doAfterTextChanged {
                 setAddButton()
             }
+            tvCreateReset.setOnClickListener {
+                // 입력 데이터 초기화
+                startTime = null
+                endTime = null
+                btnCreateStartDate.text = "시작 시간"
+                btnCreateEndDate.text = "종료 시간"
+                cbCreateOffline.isChecked = false
+                cbCreateOnline.isChecked = false
+                etCreatePlace.apply {
+                    isClickable = false
+                    setText("")
+                    hint = resources.getString(R.string.create_place_hint)
+                }
+                etCreateDetail.apply {
+                    setText("")
+                    hint = resources.getString(R.string.create_detail)
+                }
+            }
             btnCreateAdd.setOnClickListener {
                 // Todo : ViewModel 에서 post
             }
@@ -120,25 +139,63 @@ class RoundCreateActivity : AppCompatActivity() {
                         wrapSelectorWheel = false
                         descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
                         with(LocalDateTime.now()) {
-                            // 오늘인 경우
                             val minuteIdx = minute / 10
-                            minValue = if (year == dateYear && monthValue == monthOfYear + 1 && day == dayOfMonth) {
-                                if (minuteIdx == 5 && minute % 10 > 0){
+                            // 시작 시간보다 종료 시간이 더 뒤에 있어야 함
+                            minValue = if (!isStart && startTime != null && startTime?.year == dateYear && startTime?.month == monthOfYear + 1 && startTime?.day == dayOfMonth) {
+                                if (startTime?.minute == 50) startTime?.hourOfDay!! + 1
+                                else startTime?.hourOfDay!!
+                            }
+                            // 오늘인 경우
+                            else if (year == dateYear && monthValue == monthOfYear + 1 && day == dayOfMonth) {
+                                if (!isStart && startTime == null){
+                                    // 현재 시각이 40분이상인 경우 시작 시간이 50분 이상이기 때문에 회차의 최소시간을 고려하여 다음 시간으로 넘겨야함
+                                    if (minuteIdx >= 4) {
+                                        hour + 1.also {
+                                            value = hour + 1
+                                        }
+                                    }
+                                    else  hour .also {
+                                            value = hour
+                                        }
+                                }
+                                else if (minuteIdx == 5 && minute % 10 > 0){
                                     hour + 1 .also {
                                         value = hour + 1
                                     }
-                                }
-                                else hour .also {
+                                } else hour .also {
                                     value = hour
                                 }
                             } else 0
-                            maxValue = 23
+                            maxValue = if (isStart && endTime != null && endTime?.year == dateYear && endTime?.month == monthOfYear + 1 && endTime?.day == dayOfMonth){
+                                if (endTime?.minute == 0) endTime?.hourOfDay!! - 1
+                                else endTime?.hourOfDay!!
+                            } else 23
 
                             setOnValueChangedListener { _, _, newVal ->
                                 dialogBinding.npMinutePicker.apply{
                                     value = minValue
-                                    if (year == dateYear && monthValue == monthOfYear + 1 && day == dayOfMonth) {
+                                    if (!isStart && startTime != null && startTime?.year == dateYear && startTime?.month == monthOfYear + 1 && startTime?.day == dayOfMonth){
+                                        if (newVal == startTime?.hourOfDay){
+                                            minValue = (startTime?.minute!! / 10) + 1
+                                            displayedValues = arrayOf("0", "10", "20", "30", "40", "50").sliceArray(minValue..5)
+                                        } else {
+                                            displayedValues = arrayOf("0", "10", "20", "30", "40", "50")
+                                            minValue = 0
+                                        }
                                         maxValue = 5
+                                    }else if (isStart && endTime != null && dateYear == endTime?.year && monthOfYear + 1 == endTime?.month && dayOfMonth == endTime?.day) {
+                                        if (endTime?.hourOfDay == newVal) {
+                                            maxValue = (endTime?.minute!! / 10) - 1
+                                            displayedValues =
+                                                arrayOf("0", "10", "20", "30", "40", "50").sliceArray(
+                                                    0..maxValue
+                                                )
+                                        } else {
+                                            maxValue = 5
+                                            displayedValues = arrayOf("0", "10", "20", "30", "40", "50")
+                                        }
+                                    }
+                                    else if (year == dateYear && monthValue == monthOfYear + 1 && day == dayOfMonth) {
                                         if (newVal.toString() == hour.toString()) {
                                             minValue = minuteIdx + 1
                                             displayedValues = arrayOf("0", "10", "20", "30", "40", "50").sliceArray(minuteIdx + 1..5)
@@ -147,7 +204,9 @@ class RoundCreateActivity : AppCompatActivity() {
                                             value = 0
                                             minValue = 0
                                         }
+                                        maxValue = 5
                                     }
+
                                 }
                             }
                         }
@@ -157,14 +216,51 @@ class RoundCreateActivity : AppCompatActivity() {
                         wrapSelectorWheel = false
                         descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
                         with(LocalDateTime.now()) {
-                            if (year != dateYear || monthValue != monthOfYear + 1 || day != dayOfMonth) {
+                            if (!isStart && startTime != null && dateYear == startTime?.year && monthOfYear + 1 == startTime?.month && dayOfMonth == startTime?.day) {
+                                // 시작 시간의 날짜와 종료 시간의 날짜가 동일한 경우
+                                if (startTime?.hourOfDay.toString() == dialogBinding.npHourPicker.value.toString()) {
+                                    minValue = (startTime?.minute!! / 10) + 1
+                                    maxValue = 5
+                                    displayedValues =
+                                        arrayOf("0", "10", "20", "30", "40", "50").sliceArray(
+                                            minValue..5
+                                        )
+                                } else {
+                                    minValue = 0
+                                    maxValue = 5
+                                    displayedValues = arrayOf("0", "10", "20", "30", "40", "50")
+                                }
+                            }else if (isStart && endTime != null && dateYear == endTime?.year && monthOfYear + 1 == endTime?.month && dayOfMonth == endTime?.day) {
+                                if (endTime?.hourOfDay.toString() == dialogBinding.npHourPicker.value.toString()) {
+                                    maxValue = (endTime?.minute!! / 10) - 1
+                                    displayedValues =
+                                        arrayOf("0", "10", "20", "30", "40", "50").sliceArray(
+                                            0..maxValue
+                                        )
+                                } else {
+                                    maxValue = 5
+                                    displayedValues = arrayOf("0", "10", "20", "30", "40", "50")
+                                }
+                            }
+                            else if (year != dateYear || monthValue != monthOfYear + 1 || day != dayOfMonth) {
                                 minValue = 0
                                 displayedValues = arrayOf("0", "10", "20", "30", "40", "50")
-                            } else if (year == dateYear && monthValue == monthOfYear + 1 && day == dayOfMonth && hour.toString() == dialogBinding.npHourPicker.value.toString()){
-                                minValue = (minute / 10) + 1
-                                displayedValues = arrayOf("0", "10", "20", "30", "40", "50").sliceArray(minute / 10 + 1..5)
+                                maxValue = 5
                             }
-                            maxValue = 5
+                            else if (year == dateYear && monthValue == monthOfYear + 1 && day == dayOfMonth && hour.toString() == dialogBinding.npHourPicker.value.toString()){
+                                minValue = if (isStart) minute / 10 + 1 else minute / 10 + 2
+                                displayedValues = arrayOf("0", "10", "20", "30", "40", "50").sliceArray(minValue..5)
+                                maxValue = 5
+                            } else if (!isStart && year == dateYear && monthValue == monthOfYear + 1 && day == dayOfMonth && (hour+1).toString() == dialogBinding.npHourPicker.value.toString()){
+                                minValue = 1
+                                displayedValues = arrayOf("0", "10", "20", "30", "40", "50").sliceArray(1..5)
+                                maxValue = 5
+                            }
+                            else {
+                                minValue = 0
+                                displayedValues = arrayOf("0", "10", "20", "30", "40", "50")
+                                maxValue = 5
+                            }
                         }
                     }
                     AlertDialog.Builder(this@RoundCreateActivity)
@@ -177,14 +273,26 @@ class RoundCreateActivity : AppCompatActivity() {
                                     "시작 : ${dateYear}.${monthOfYear + 1}.${dayOfMonth} ${dialogBinding.npHourPicker.value}:%02d",
                                     (dialogBinding.npMinutePicker.value) * 10
                                 )
-                                startTime = DateTime(dateYear, monthOfYear, dayOfMonth, dialogBinding.npHourPicker.value, dialogBinding.npMinutePicker.value * 10)
+                                startTime = DateTime(dateYear, monthOfYear + 1, dayOfMonth, dialogBinding.npHourPicker.value, dialogBinding.npMinutePicker.value * 10)
+
+                                // 종료 시간이 설정 되어 있을 때 새롭게 설정된 시작 시간이 종료 시간 보다 같거나 느린 경우(종료 시간이 더 빠름) 종료 시간 초기화
+                                if (compareStartEndTime() == 2 || compareStartEndTime() == 3){
+                                    btnCreateEndDate.text = "종료 시간"
+                                    endTime = null
+                                }
                             }
                             else {
                                 btnCreateEndDate.text = String.format(
                                     "종료 : ${dateYear}.${monthOfYear + 1}.${dayOfMonth} ${dialogBinding.npHourPicker.value}:%02d",
                                     (dialogBinding.npMinutePicker.value) * 10
                                 )
-                                endTime = DateTime(dateYear, monthOfYear, dayOfMonth, dialogBinding.npHourPicker.value, dialogBinding.npMinutePicker.value * 10)
+                                endTime = DateTime(dateYear, monthOfYear + 1, dayOfMonth, dialogBinding.npHourPicker.value, dialogBinding.npMinutePicker.value * 10)
+
+                                // 종료 시간이 설정 되어 있을 때 새롭게 설정된 시작 시간이 종료 시간 보다 같거나 느린 경우(종료 시간이 더 빠름) 종료 시간 초기화
+                                if (compareStartEndTime() == 2 || compareStartEndTime() == 3){
+                                    btnCreateStartDate.text = "시작 시간 "
+                                    startTime = null
+                                }
                             }
                         }
                         .setNegativeButton("취소") { _, _
@@ -192,7 +300,21 @@ class RoundCreateActivity : AppCompatActivity() {
                         .create()
                         .show()
                 }
-                datePicker.minDate = System.currentTimeMillis() - 1000
+                if(isStart || startTime == null){
+                    datePicker.minDate = System.currentTimeMillis() - 1000
+                } else {
+                    startTime?.let{
+                        calendar.set(startTime?.year!!, startTime?.month!! - 1, startTime?.day!!)
+                        datePicker.minDate = calendar.timeInMillis
+                    }
+                }
+
+                if(isStart){
+                    endTime?.let{
+                        calendar.set(endTime?.year!!, endTime?.month!! - 1, endTime?.day!!)
+                        datePicker.maxDate = calendar.timeInMillis
+                    }
+                }
                 show()
             }
         }
@@ -207,7 +329,22 @@ class RoundCreateActivity : AppCompatActivity() {
         }
     }
 
-
+    private fun compareStartEndTime() : Int {
+        // 0 : 둘다 null
+        // 1 : startTime 이 더 빠름 (startTime 의 값이 더 작음)
+        // 2 : endTime 이 더 빠름 (이 경우 로직 수정해야 함)
+        // 3 : 둘이 같음
+        startTime?.let{
+            endTime?.let{
+                val startTimeToLong = String.format("%4d%02d%02d%02d%02d", startTime?.year, startTime?.month, startTime?.day, startTime?.hourOfDay, startTime?.minute).toLong()
+                val endTimeToLong = String.format("%4d%02d%02d%02d%02d", endTime?.year, endTime?.month, endTime?.day, endTime?.hourOfDay, endTime?.minute).toLong()
+                return if (startTimeToLong < endTimeToLong) 1
+                else if (startTimeToLong > endTimeToLong) 2
+                else 3
+            }
+        }
+        return 0
+    }
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
         if (currentFocus != null) {
             val rect = Rect()
