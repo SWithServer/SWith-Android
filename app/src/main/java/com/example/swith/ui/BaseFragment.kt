59a6b8aa8fbdb7aea5d5807cb07bd70db172a4d0
nerw173 <nerw173@gmail.com>
2022-07-26 23:32:37 +0900

    [UPD] 출석 화면 업데이트

--- app/src/main/java/com/example/swith/ui/BaseFragment.kt
+++ app/src/main/java/com/example/swith/ui/BaseFragment.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment<VB : ViewBinding>(private val layoutRes: Int): Fragment(){
+abstract class BaseFragment<VB : ViewBinding>(@LayoutRes private val layoutRes: Int): Fragment(){
     private var _viewBinding: VB? = null
     protected val binding get() = _viewBinding!!
 
--- app/src/main/java/com/example/swith/ui/study/round/RoundAttendFragment.kt
+++ app/src/main/java/com/example/swith/ui/study/round/RoundAttendFragment.kt
@@ -1,18 +1,21 @@
 package com.example.swith.ui.study.round
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.studywith.ConfirmDialog
+import com.example.studywith.ConfirmDialogInterface
 import com.example.swith.R
 import com.example.swith.databinding.FragmentRoundAttendBinding
 import com.example.swith.ui.BaseFragment
 import com.example.swith.ui.adapter.AttendRVAdapter
 import com.example.swith.viewmodel.AttendViewModel
 
-class RoundAttendFragment : BaseFragment<FragmentRoundAttendBinding>(R.layout.fragment_round_attend) {
+class RoundAttendFragment : BaseFragment<FragmentRoundAttendBinding>(R.layout.fragment_round_attend){
     private val viewModel: AttendViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -22,8 +25,10 @@ class RoundAttendFragment : BaseFragment<FragmentRoundAttendBinding>(R.layout.fr
 
     private fun initView(){
         viewModel.setCurUser(5)
+        setVisibility(viewModel.isUpdateAvailable(), viewModel.attendLiveData.value == null)
 
         viewModel.attendLiveData.observe(viewLifecycleOwner, Observer {
+            setVisibility(viewModel.isUpdateAvailable(), viewModel.attendLiveData.value == null)
             (binding.rvAttend.adapter as AttendRVAdapter).setData(it)
         })
 
@@ -33,10 +38,30 @@ class RoundAttendFragment : BaseFragment<FragmentRoundAttendBinding>(R.layout.fr
                 adapter = AttendRVAdapter()
                 layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
             }
-            btnAttend.visibility = if (viewModel.isUpdateAvailable()) View.VISIBLE else View.INVISIBLE
             btnAttend.setOnClickListener {
-                viewModel.updateCurAttend()
+                ConfirmDialog("출석 하시겠습니까?").apply {
+                    setMyDialogInterface(this, object: ConfirmDialogInterface {
+                        override fun onYesButtonClick() {
+                            viewModel.updateCurAttend()
+                        }
+                    })
+                }.show(requireActivity().supportFragmentManager, "attendDialog")
             }
         }
     }
+
+    private fun setVisibility(updateAvail: Boolean, isNull: Boolean){
+        with(binding) {
+            btnAttend.visibility = if (updateAvail) View.VISIBLE else View.INVISIBLE
+            svAttend.visibility = if (isNull) View.INVISIBLE else View.VISIBLE
+            tvNoPeople.visibility = if (isNull) View.VISIBLE else View.INVISIBLE
+        }
+    }
+
+    // 외부에서 인터페이스 리스너 삽입
+    private fun setMyDialogInterface(dialog: ConfirmDialog, myDialogInterface: ConfirmDialogInterface){
+        dialog.confirmDialogInterface = myDialogInterface
+    }
+
+    // Todo : 10분 넘었을 때 지각으로 처리되는 로직도 추가해야함
 }
