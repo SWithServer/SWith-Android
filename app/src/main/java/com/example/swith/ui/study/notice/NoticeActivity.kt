2f6fb06f55b67c0def64ed07b460b05d9ed4fb81
kimdoori <dooooreeee@naver.com>
2022-08-03 17:59:05 +0900

    [ADD] NoticeManager

--- app/src/main/java/com/example/swith/ui/study/notice/NoticeActivity.kt
+++ app/src/main/java/com/example/swith/ui/study/notice/NoticeActivity.kt
@@ -3,18 +3,31 @@ package com.example.swith.ui.study.notice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.view.Window
 import androidx.databinding.DataBindingUtil
 import com.example.swith.R
 import com.example.swith.databinding.ActivityNoticeBinding
+import com.example.swith.utils.NoticeManager
 
-class NoticeActivity : AppCompatActivity() {
+class NoticeActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var binding: ActivityNoticeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_notice)
 
         initToolbar()
+
+        initData()
+        initView()
+    }
+
+    private fun initView() {
+        binding.clickListener=this@NoticeActivity
+    }
+
+    private fun initData() {
+        //공지사항 가져오기
     }
 
     private fun initToolbar(){
@@ -28,5 +41,14 @@ class NoticeActivity : AppCompatActivity() {
             android.R.id.home -> finish()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.btn_test ->{
+                //TODO only test, pushService만들면 로직 삭제
+                NoticeManager(this@NoticeActivity).runNotice("알람제목","알람테스트")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/swith/utils/NoticeManager.kt
@@ -0,0 +1,65 @@
+package com.example.swith.utils
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.content.Intent
+import android.graphics.Color
+import androidx.core.app.NotificationCompat
+import com.example.swith.ui.MainActivity
+import com.example.swith.ui.study.notice.NoticeActivity
+
+
+class NoticeManager(private val context: Context) {
+    companion object {
+        private const val CHANNEL_ID = "notice"
+        private const val CHANNEL_NAME: String = "noticeChannel"
+        private const val CHANNEL_NUMBER = 11
+    }
+
+    var manager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    private lateinit var builder: NotificationCompat.Builder
+
+    init {
+        createChannel()
+
+    }
+
+    private fun createChannel() {
+        val channelId = CHANNEL_ID
+        val channelName = CHANNEL_NAME
+        val channel = NotificationChannel(
+            channelId,
+            channelName,
+            NotificationManager.IMPORTANCE_HIGH
+        )
+
+        channel.setShowBadge(true)
+        //TODO 알람소리, 진동은 어떻게 할 것 인가?
+        channel.enableLights(true)
+        channel.lightColor = Color.RED
+
+        manager.createNotificationChannel(channel)
+
+        builder = NotificationCompat.Builder(context, channelId)
+
+    }
+
+    fun runNotice(title: String,content: String){
+        val actionIntent = Intent(context,NoticeActivity::class.java)
+        val actionPendingIntent = PendingIntent.getActivity(context, CHANNEL_NUMBER,actionIntent,PendingIntent.FLAG_IMMUTABLE)
+
+        //TODO 임시아이콘
+        builder.run {
+            setSmallIcon(android.R.mipmap.sym_def_app_icon)
+            setWhen(System.currentTimeMillis())
+            setContentTitle(title)
+            setContentText(content)
+            setContentIntent(actionPendingIntent)
+
+            manager.notify(CHANNEL_NUMBER,build())
+        }
+    }
+}
