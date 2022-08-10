e9e9244e32e07b3c0142853f05a2461610a8e67b
nerw173 <nerw173@gmail.com>
2022-08-10 22:04:12 +0900

    [UPD] 다이얼로그 크기 변경

--- app/src/main/java/com/example/swith/ui/dialog/CustomAlertDialog.kt
+++ app/src/main/java/com/example/swith/ui/dialog/CustomAlertDialog.kt
@@ -1,9 +1,11 @@
 package com.example.swith.ui.dialog
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import com.example.swith.R
@@ -12,12 +14,26 @@ import com.example.swith.databinding.DialogAlertBinding
 class CustomAlertDialog(private val title: String, private val content: String) : DialogFragment() {
     private lateinit var binding: DialogAlertBinding
 
+    override fun onResume() {
+        super.onResume()
+
+        // 디바이스 크기별 세팅
+        val params = dialog?.window?.attributes
+        val windowManager = activity?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val size = windowManager.currentWindowMetrics
+        val deviceWidth = size.bounds.width()
+
+        params?.width = (deviceWidth * 0.8).toInt()
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.dialog_alert, container, false)
+        isCancelable = false
         return binding.root
     }
 
