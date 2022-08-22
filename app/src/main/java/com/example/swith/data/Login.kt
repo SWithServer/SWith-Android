ace1bc2bd714a3be712876e466d335c32bd921f1
kimdoori <dooooreeee@naver.com>
2022-08-22 16:03:46 +0900

    [ADD] logindata

---
+++ app/src/main/java/com/example/swith/data/Login.kt
@@ -0,0 +1,17 @@
+package com.example.swith.data
+
+data class Login(
+    val accessToken: String,
+    val averageStar: Any,
+    val email: String,
+    val interestIdx1: Any,
+    val interestIdx2: Any,
+    val introduction: Any,
+    val isSignUp: Boolean,
+    val nickname: String,
+    val profileImgUrl: String,
+    val refreshToken: String,
+    val role: String,
+    val status: Int,
+    val userIdx: Int
+)
---
+++ app/src/main/java/com/example/swith/data/LoginRequest.kt
@@ -0,0 +1,7 @@
+package com.example.swith.data
+
+data class LoginRequest(
+    val email: String,
+    val nickname: String,
+    val profileImgUrl: String
+)
